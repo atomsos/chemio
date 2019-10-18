3c39476ff9fa8ae4c2ef83589f6dce10cdad13fe
Sky Zhang <sky.atomse@gmail.com>
2019-10-18 12:43:04 +0800

    update

--- chemio/cli/utils.py
+++ chemio/cli/utils.py
@@ -25,7 +25,6 @@ def parse_args_data(args_data):
             else:
                 raise ValueError("args data: d not a=b format")
 
-    import pdb; pdb.set_trace()
     print('data:', data)
     return data
 
